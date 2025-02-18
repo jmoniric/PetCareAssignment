c95616cc97e389d28fdc0cac1f9122c09623fd36
Jmonipina <162907373+Jmonipina@users.noreply.github.com>
2025-02-18 16:34:27 -0500

    Created a small folder structure in 'Content' also added a sprite for 'Menu Buttons'

--- PetCareGame/PetCareGame/obj/Debug/net8.0/PetCareGame.AssemblyInfo.cs
+++ PetCareGame/PetCareGame/obj/Debug/net8.0/PetCareGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetCareGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+735993657aa946934b047c91c22abb80903de1e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93936c4c9ed492d144d53ee527d071faa53ac3c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetCareGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetCareGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
