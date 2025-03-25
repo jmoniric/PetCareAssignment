e4a898c3b61168630701bd23dcb41d98bda2c1cd
Jmonipina <162907373+Jmonipina@users.noreply.github.com>
2025-03-25 13:16:44 -0400

    Changed SaveFile class to use static variables

--- PetCareGame/PetCareGame/Game/SaveFile.cs
+++ PetCareGame/PetCareGame/Game/SaveFile.cs
@@ -1,14 +1,30 @@
 using System;
+using System.IO;
+using System.Text.Json;
 
 namespace PetCareGame
 {
     public class SaveFile{
-        public bool BathDone { get; set; }
-        public bool NailTrimDone { get; set; }
-        public bool BrushingDone { get; set; }
-        public bool PetCareDone { get; set; }
+        public static bool BathDone { get; set; }
+        public static bool NailTrimDone { get; set; }
+        public static bool BrushingDone { get; set; }
+        public static bool PetCareDone { get; set; }
 
-        public bool SlidingGameDone { get; set; }
-        public bool WheresWaldoDone { get; set; }
+        public static bool SlidingGameDone { get; set; }
+        public static bool WheresWaldoDone { get; set; }
+
+        private const string PATH = "stats.json";
+
+        public void Save(SaveFile saved)
+        {
+            string serializedText = JsonSerializer.Serialize<SaveFile>(saved);
+            File.WriteAllText(PATH, serializedText);
+        }
+
+        public SaveFile Load()
+        {
+            var fileContents = File.ReadAllText(PATH);
+            return JsonSerializer.Deserialize<SaveFile>(fileContents);
+        }
     }
 }
